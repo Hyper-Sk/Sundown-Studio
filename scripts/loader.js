8f5553779190fab7df7277bea59f341524880268
crammaze <crammaze@crammazes-Mac-mini.local>
2024-12-17 10:29:41 +0530

    fourth

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,7 +1,24 @@
-let loader = document.querySelector('.loader')
-window.addEventListener("load", () => {
+function loader1() {
+  let loader1 = document.querySelector(".loader1");
+  window.addEventListener("load", () => {
     setTimeout(() => {
-        loader.style.top = '-130%'
-    },4000)
-    
-})
+      loader1.style.top = "-130%";
+    }, 4000);
+  });
+}
+loader1();
+
+function loader2() {
+  let loader2 = document.querySelector(".loader2");
+  gsap.to(loader2, {
+      y:0
+  })
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+        gsap.to(loader2, {
+            y:"-100dvh"
+        })
+    }, 1000);
+  });
+}
+loader2();
